6b0deabed9b2d1e5c2459e6ca3a770fe2e256f7c
David-Mattar <david.mattar2@gmail.com>
2023-01-25 22:49:25 -0300

    Update and new tasks

--- lib/pages/auth/login_page.dart
+++ lib/pages/auth/login_page.dart
@@ -8,9 +8,28 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  
+  final formKey = GlobalKey<FormState>();
+
   Widget build(BuildContext context){
     return Scaffold(
-      body: Center(child: Text("LoginPage")),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 80),
+          child: Form(
+            key: formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: const <Widget> [
+                Text("Groupie", style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),),
+                SizedBox(height: 10),
+                Text("Login now to see what they are talking!", style: TextStyle(fontSize: 15, fontWeight: FontWeight.w400),)
+                ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
